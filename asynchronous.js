b74133c020f78f7bdc44ad6143cafccb422698ca
Sagar Kumar <mtech.sagarkumar@gmail.com>
2021-10-01 09:58:58 +0530

    sync async

---
+++ asynchronous.js
@@ -0,0 +1,14 @@
+//Asynchronous
+
+console.log("I")
+
+//setTimeout function is inbuilt function in js
+//Arrow function
+//Time in ms
+setTimeout(() => {
+    console.log("Kumar")
+}, 1000);
+
+
+console.log("am")
+console.log("Sagar")
---
+++ synchronous.js
@@ -0,0 +1,8 @@
+//Synchronous
+
+//Javascript runs from top to bottom
+
+console.log("I")
+console.log("am")
+console.log("Sagar")
+console.log("Kumar")
